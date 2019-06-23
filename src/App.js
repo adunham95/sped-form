3cbefd2707219d4ca5181535f355cec33610dcca
Adrian <adunham95@gmail.cooom>
2019-06-23 18:06:43 -0400

    Added Check

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Elements, StripeProvider } from 'react-stripe-elements';
 import items from './api/items';
 import Product from './components/Product/Product';
+import Cart from './components/Cart/Cart';
+import CheckoutForm from './components/CheckoutForm/CheckoutForm';
 import logo from './logo.svg';
 import './App.css';
 
 export default function App() {
+  const [itemsInCart, setItemsInCart] = useState([]);
+
+  const handleAddToCartClick = id => {
+    setItemsInCart(itemsInCart => {
+      const itemInCart = itemsInCart.find(item => item.id === id);
+
+      // if item is already in cart, update the quantity
+      if (itemInCart) {
+        return itemsInCart.map(item => {
+          if (item.id !== id) return item;
+          return { ...itemInCart, quantity: item.quantity + 1 };
+        });
+      }
+
+      // otherwise, add new item to cart
+      const item = items.find(item => item.id === id);
+      return [...itemsInCart, { ...item, quantity: 1 }];
+    });
+  };
+
+  const totalCost = itemsInCart.reduce(
+      (acc, item) => acc + item.price * item.quantity,
+      0
+  );
+
   return (
       <div className="App">
         <header className="App-header">
@@ -14,9 +42,22 @@ export default function App() {
         <main className="App-shop">
           <div className="App-products">
             {items.map(item => (
-                <Product key={item.title} title={item.title} price={item.price} />
+                <Product
+                    key={item.title}
+                    title={item.title}
+                    price={item.price}
+                    onAddToCartClick={() => handleAddToCartClick(item.id)}
+                />
             ))}
           </div>
+          <Cart itemsInCart={itemsInCart} totalCost={totalCost} />
+          {itemsInCart.length > 0 && (
+              <StripeProvider apiKey="your_public_key">
+                <Elements>
+                  <CheckoutForm totalCost={totalCost} />
+                </Elements>
+              </StripeProvider>
+          )}
         </main>
       </div>
   );
